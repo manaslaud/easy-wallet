6d25a751837582b8fd7a295b9776c0480a72a13b
Manas Laud <manaslaud2004@gmail.com>
2025-04-26 23:18:48 +0530

    feat: ethereum base keypair

---
+++ apps/web/src/helpers/ethereum/createEthereumKeys.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ethers } from "ethers";
+import { mnemonicToSeedSync } from "../bip39";
+import { KeyPair } from "@/types/account";
+import { encrypt } from "../encryption";
+import { _BASE_ETHEREUM_WALLET_PATH } from "@/constants/wallet";
+
+//creating the base keys
+export async function createBaseEthereumKeys(
+  mneuonic: string,
+  passowrd: string
+) {
+  //   const seed = mnemonicToSeedSync(mneumonic, passowrd);
+  const ethersCompatibleMnemonic = ethers.Mnemonic.fromPhrase(mneuonic);
+  const wallet = ethers.HDNodeWallet.fromMnemonic(
+    ethersCompatibleMnemonic,
+    _BASE_ETHEREUM_WALLET_PATH
+  );
+  const encryptedPvtKey = (await encrypt(passowrd, wallet.privateKey)).res;
+  const keypair: KeyPair = {
+    publicKey: wallet.publicKey,
+    privateKey: encryptedPvtKey, // encrypted
+    derivationPath: _BASE_ETHEREUM_WALLET_PATH,
+    chain: "ethereum",
+    address: wallet.address,
+  };
+  return keypair;
+}
