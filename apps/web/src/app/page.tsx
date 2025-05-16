1c82e970887edec381ce06c21f26971eadb99833
Biraj Kashyap <kashyapbiraj83@gmail.com>
2025-05-16 11:40:39 +0530

    Component files restored

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,6 @@
 "use client";
-// import { createBaseAccount } from "@/helpers/createAccount";
-// import { useEffect } from "react";
-// export default function Home() {
-//   useEffect(() => {
-//     const f = async () => {
-//       const password = "12345678";
-//       const label = "manas account";
-//       console.log(await createBaseAccount(password, label));
-//     };
-//     f();
-//   }, []);
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-//       <h1 className="text-4xl font-bold">Welcome to the web app!</h1>
-//       <p className="mt-4 text-lg">This is a simple web application.</p>
-//     </main>
-//   );
-//}
-"use client";
+import { createBaseAccount } from "@/helpers/createAccount";
+import { useEffect } from "react";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/layout/Navbar";
@@ -30,6 +13,14 @@ import Pricing from "@/components/sections/Pricing";
 import Footer from "@/components/layout/Footer";
 
 export default function Home() {
+  useEffect(() => {
+    const f = async () => {
+      const password = "12345678";
+      const label = "manas account";
+      console.log(await createBaseAccount(password, label));
+    };
+    f();
+  }, []);
   return (
     <ThemeProvider
       attribute="class"
